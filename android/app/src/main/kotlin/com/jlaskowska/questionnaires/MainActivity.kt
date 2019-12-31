da988be1113725dbbd6a3ffb0a2b48d52de4ea44
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2019-12-31 12:53:28 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/jlaskowska/questionnaires/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jlaskowska.questionnaires
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
