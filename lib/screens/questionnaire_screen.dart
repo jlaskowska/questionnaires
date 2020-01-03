aa02544623b7383375300bdf9043a76e2556f5a8
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-01-03 14:26:42 +0100

    Added grouped_buttons package. Proof of concept QuestionnaireScreen.

--- lib/screens/questionnaire_screen.dart
+++ lib/screens/questionnaire_screen.dart
@@ -1,16 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:grouped_buttons/grouped_buttons.dart';
+import 'package:questionnaires/models/question.dart';
 import 'package:questionnaires/models/questionnaire.dart';
 
-class QuestionnaireScreen extends StatelessWidget {
+class QuestionnaireScreen extends StatefulWidget {
   final Questionnaire questionnaire;
 
   QuestionnaireScreen({@required this.questionnaire});
 
   @override
+  _QuestionnaireScreenState createState() => _QuestionnaireScreenState();
+}
+
+class _QuestionnaireScreenState extends State<QuestionnaireScreen> {
+  List<Question> get questions => widget.questionnaire.questions;
+  int questionIndex;
+  Question get currentQuestion => questions[questionIndex];
+
+  @override
+  void initState() {
+    super.initState();
+
+    questionIndex = 0;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
       appBar: AppBar(
-        title: Text(questionnaire.name),
+        title: Text(widget.questionnaire.name),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Card(
+            color: Colors.white,
+            child: Column(
+              children: <Widget>[
+                Text(currentQuestion.text),
+                RadioButtonGroup(
+                  labels: currentQuestion.answers.map((answer) => answer.text).toList(),
+                  onChange: (_, index) => print(index),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    RaisedButton(
+                      child: Text('Back'),
+                      onPressed: null,
+                    ),
+                    RaisedButton(
+                      child: Text('Next'),
+                      onPressed: null,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
