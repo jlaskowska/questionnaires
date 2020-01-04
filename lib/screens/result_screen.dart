5d1132a3ff3112ef71b8b169815d2cebe7f4773c
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-01-04 17:13:36 +0100

    Created ResultScreen.

---
+++ lib/screens/result_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:questionnaires/widgets/button.dart';
+
+class ResultScreen extends StatelessWidget {
+  final String questionnaireName;
+  final String interpretation;
+
+  ResultScreen({@required this.questionnaireName, this.interpretation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(questionnaireName),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return Container(
+                width: constraints.maxWidth * 0.75,
+                height: constraints.maxHeight * 0.5,
+                child: Card(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 40.0, bottom: 20),
+                        child: Text(
+                          'Your Result:',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                        ),
+                      ),
+                      Text('Interpretation'),
+                      SizedBox(height: 60),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: Button.primary(
+                          buttonLabel: 'Main Menu',
+                          onPressed: () => Navigator.of(context).pop(),
+                        ),
+                      ),
+                    ],
+                    //
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
